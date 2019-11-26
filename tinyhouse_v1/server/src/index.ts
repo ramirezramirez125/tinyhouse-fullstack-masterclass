2cb4f8707eaa58c56e99b9d1016b9fc1e673041d
Paulina Grunwald <paulina.grunwald@gmail.com>
2019-11-26 07:55:28 +0100

    Add dependencies. Add mock listing. Create get and post express routes.

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,10 +1,24 @@
 
 import express from 'express'
+import bodyParser from 'body-parser'
+import { listings } from "./listings"
 const app = express()
 const port = 9000
 
-app.get('/', (_req,res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json())
+
+app.get("/listings", (_req, res) => {
+  res.send(listings);
+});
+
+app.post("/delete-listing", (_req, res) => {
+  const id = _req.body.id
+  for (let index = 0; index < listings.length; index++) {
+    if(listings[index].id === id) {
+      return res.send(listings.splice(index, 1))
+    }
+    return res.send("Failed to delete listing.")
+  }
 })
 
 app.listen(port)
