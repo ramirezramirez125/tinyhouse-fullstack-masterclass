fa16f53be9dee06450efcd0809d731252b71d3f4
Paulina Grunwald <paulina.grunwald@gmail.com>
2019-11-27 08:58:55 +0100

    Add apollo  sever. Set up schema.

--- tinyhouse_v1/server/src/index.ts
+++ tinyhouse_v1/server/src/index.ts
@@ -1,25 +1,12 @@
 
 import express from 'express'
-import bodyParser from 'body-parser'
-import { listings } from "./listings"
+import { ApolloServer } from 'apollo-server-express'
+import { schema } from './graphql'
 const app = express()
 const port = 9000
 
-app.use(bodyParser.json())
-
-app.get("/listings", (_req, res) => {
-  res.send(listings);
-});
-
-app.post("/delete-listing", (_req, res) => {
-  const id = _req.body.id
-  for (let index = 0; index < listings.length; index++) {
-    if(listings[index].id === id) {
-      return res.send(listings.splice(index, 1))
-    }
-    return res.send("Failed to delete listing.")
-  }
-})
+const server = new ApolloServer({schema})
+server.applyMiddleware({app, path: '/api'})
 
 app.listen(port)
 console.log(`[app]: http://localhost:${port}`)
